494a0177438d4587682dc923ab942910c46c3874
Kaan Ersoy <kaan.ersoy@trendyol.com>
2021-10-15 20:50:35 +0300

    Add get and update notes routes and controllers

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,4 +1,5 @@
-const db = require('../db');
+const { NotesModel } = require('../db');
+const { createErrorMessage } = require('../utils/createMessage');
 
 const addNoteController = (req, res) => {
   const { title, content } = req.body;
@@ -7,7 +8,7 @@ const addNoteController = (req, res) => {
     title,
     content,
   };
-  db.notes.insert(newNote, (err, newDoc) => {
+  NotesModel.create(newNote, (err, newDoc) => {
     if (err) {
       return res.status(400).send({
         success: false,
@@ -21,11 +22,47 @@ const addNoteController = (req, res) => {
   });
 };
 
+const updateNoteWithIdController = (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+
+  NotesModel.findOneAndUpdate({ _id: id }, { title, content }, (err, updatedDoc) => {
+    if (err) {
+      return res.status(400).send({
+        ...createErrorMessage(`NoteNotUpdatedWithId: ${id}`),
+        err,
+      });
+    }
+    return res.send({
+      success: true,
+      data: updatedDoc,
+    });
+  });
+};
+
 const getNotesController = (req, res) => {
-  res.send(db.notes.getAllData());
+  NotesModel.find({}, (err, docs) => {
+    if (err) {
+      res.send({ ...createErrorMessage('ConnotGetNotes'), err });
+    }
+    res.send(docs);
+  });
+};
+
+const getNoteWithIdController = (req, res) => {
+  const { id } = req.params;
+
+  NotesModel.find({ _id: id }, (err, docs) => {
+    if (err) {
+      res.send({ ...createErrorMessage('ConnotGetNotes'), err });
+    }
+    res.send(docs);
+  });
 };
 
 module.exports = {
   addNoteController,
   getNotesController,
+  updateNoteWithIdController,
+  getNoteWithIdController,
 };
--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
-const { getNotesController, addNoteController } = require('../controllers/notes');
-const { authRequiredMiddleware } = require('../middlewares/authMiddlewares');
+const {
+  getNotesController, addNoteController, updateNoteWithIdController, getNoteWithIdController,
+} = require('../controllers/notes');
+// const { authRequiredMiddleware } = require('../middlewares/authMiddlewares');
 
+// Get Note
 router.get('/', getNotesController);
-router.post('/', authRequiredMiddleware, addNoteController);
+router.get('/:id', getNoteWithIdController);
+
+// Add Note
+router.post('/', addNoteController);
+
+// Update Note
+router.put('/:id', updateNoteWithIdController);
 
 module.exports = router;
