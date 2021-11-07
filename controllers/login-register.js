4a69b3a97487d5cceecd7c79653e6c0125dca96e
Kaan Ersoy <kaanersoy12@gmail.com>
2021-11-07 22:54:38 +0300

    fix issue

--- controllers/login-register.js
+++ controllers/login-register.js
@@ -29,6 +29,7 @@ const loginController = (req, res) => {
   const maxAge = 36000000;
   res.cookie(USER_INFO_COOKIE_NAME, `${JSON.stringify(token)}`, {
     maxAge,
+    sameSite: 'none',
   });
   res.send({ token });
 };
@@ -36,6 +37,7 @@ const loginController = (req, res) => {
 const logOutController = (req, res) => {
   res.cookie(USER_INFO_COOKIE_NAME, '', {
     maxAge: 0,
+    sameSite: 'none',
   });
   res.send({ success: true });
 };
